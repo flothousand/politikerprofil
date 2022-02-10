3d03f2ef2a4c46ed1ad7653d6995caace49a30c7
Florian Tausend <florian.tausend@gmail.com>
2022-02-10 13:18:59 +0100

    Debug True

--- politikerprofil/settings.py
+++ politikerprofil/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#$6(n!mrdr(li9u9wdc-tx7lt$9z)=15v4z5t%@jkpc)u$$=7)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['167.99.244.192']
 
