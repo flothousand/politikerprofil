4643b361902c9b70b29b7083ecf143fd98236050
Florian Tausend <florian.tausend@gmail.com>
2022-02-12 12:14:17 +0100

    abs

--- politikerprofil/settings.py
+++ politikerprofil/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+STATIC_ROOT = '/home/sammy/politikerprofil/static_root'
+MEDIA_ROOT = '/home/sammy/politikerprofil/media_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
